3cc226d2d578de1a4a6d5f464c666032b3541246
mikaicebe <yf.yan@mail.utoronto.ca>
2016-03-04 10:25:32 +0800

    Update ZDomainScanner.py

--- Zcore/Zoutter/ZDomainScanner.py
+++ Zcore/Zoutter/ZDomainScanner.py
@@ -1,5 +1,21 @@
 #encoding:utf-8
 from Zcore import ZBaseCoreObject
+import socket
+import re
+import requests
 
 class ZDomainScanner(ZBaseCoreObject):
-    pass
+
+    def __init__(self,domain):
+        self.domain = domain
+
+    def getid(self):
+        myaddr = socket.getaddrinfo(self.domain,'http')[0][4][0]
+        return myaddr
+
+    def getpz(self):
+        r = requests.get('http://s.tool.chinaz.com/same?s='+self.domain+'&page=')
+        responseHtml = r.content
+        match = re.findall(r'<div class="w30-0 overhid"><a href=\'(.*?)\'', responseHtml)
+        return match
+
